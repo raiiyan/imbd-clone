1f08e25ae37e03e637c9e4e4309b42788e6307ae
Raiyan Hossain <raiyanhossain93@gmail.com>
2024-10-10 20:16:04 +0600

    fetch data from tmdb api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import Image from "next/image";
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+import Results from '../components/Results'
+
+type SearchParams = {
+  genre?: string;
+};
+
+
+export default async function Home({ searchParams }: { searchParams: SearchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch (
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&
+      language=en-US&page=1`
+    );
+  const data = await res.json();
+  if(!res.ok){
+    throw new Error('Faild to fetch data')
+  }
+  const results = data.results;
+  console.log(results);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Results results={results} />
+    </div>
   );
+  
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Results({results}) {
+  return (
+    <div>
+        {
+            results.map((result) => (
+                <div key={result.id}>
+                    <h2>{result.original_title}</h2>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Results
