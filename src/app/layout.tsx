c7a5727c739931eea26ac303c291458278c95c32
Raiyan Hossain <raiyanhossain93@gmail.com>
2024-10-10 19:22:48 +0600

    add navbar components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         <Providers>
           <Header />
-          <Navbar/>
+          <Navbar />
           {children}
         </Providers>
         
